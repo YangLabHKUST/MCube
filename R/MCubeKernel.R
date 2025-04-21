20f98b3363439110db70f601cdac99e892a578bf
statwangz <statwangz@gmail.com>
2025-04-21 16:06:53 +0800

    Update MCubeKernel.R

--- R/MCubeKernel.R
+++ R/MCubeKernel.R
@@ -24,7 +24,7 @@ mcubeKernel <- function(
     } else {
         spots <- intersect(rownames(coordinates), spots)
         if (length(spots) == 0) {
-            stop("mcubeKernel: no common spots between coordinates and spots!")
+            stop("mcubeKernel: No common spots between coordinates and spots!")
         }
     }
 
