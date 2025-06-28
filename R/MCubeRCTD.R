e31ebfc540b172e2e94a0474e4b00909be46fb5f
statwangz <statwangz@gmail.com>
2025-06-28 14:47:40 +0800

    Update argument description

--- R/MCubeRCTD.R
+++ R/MCubeRCTD.R
@@ -6,9 +6,9 @@
 #' run the RCTD algorithm and store the cell type deconvolution results.
 #' @param RCTD_mode A character indicating in which mode to `RCTD`, corresponding to the `doublet_mode` argument in the `spacexr::run.RCTD()` function.
 #' The choice of the `RCTD` mode depends on the resolution of the ST data and determines the analysis strategy of `MCube`.
-#' For the low resolution ST data, such as the Visium data, we recommend to use `full` mode,
-#' and `MCube` will analyze all the cell types of interest together and store the results in a single \code{\linkS4class{mcube}} object.
-#' For the high resolution ST data, such as the Visium HD and Xenium data, we recommend to use `doublet` mode.
+#' For the low resolution ST data like Visium, we recommend to use the `full` mode.
+#' Then, `MCube` will analyze all the cell types of interest together and store the results in a single \code{\linkS4class{mcube}} object.
+#' For the high resolution ST data like Visium HD and Xenium, we recommend to use the `doublet` mode.
 #' Then, `MCube` will analyze each cell type separately using the spots with a high probability of containing the target cell type according to the deconvolution results.
 #' The results will be stored in a list of \code{\linkS4class{mcube}} objects, each corresponding to a cell type.
 #' @param sample_size_max A positive integer.
