146daba2a2bb971f96563fcd9477b8d443d2c810
statwangz <statwangz@gmail.com>
2025-07-02 20:02:36 +0800

    Update pipeline figure

--- inst/figures/sticker.R
+++
@@ -1,6 +0,0 @@
-library(hexSticker) # https://github.com/GuangchuangYu/hexSticker
-sticker(
-  subplot = "inst/figures/logo.png",
-  package = NULL, p_size = 16, s_x = 1, s_y = 1, s_width = .5,
-  filename = "inst/figures/sticker.png"
-)
